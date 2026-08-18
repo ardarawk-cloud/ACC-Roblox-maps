23037a0c37d7f88117dfb7f675775daba1daa61e
ardarawk-cloud <ardarawk@gmail.com>
2026-08-19 00:34:01 +0800

    Switch BBYA runtime to clean front lobby v4.9

--- scripts/inject-bbya.js
+++ scripts/inject-bbya.js
@@ -4,91 +4,77 @@ const path = require('path');
 const mapId = process.argv[2];
 if (mapId !== 'a-club') process.exit(0);
 
-const registry = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'maps/registry.json'), 'utf8'));
+const root = process.cwd();
+const registry = JSON.parse(fs.readFileSync(path.join(root, 'maps/registry.json'), 'utf8'));
 const target = registry.maps?.[mapId];
 if (!target) throw new Error(`Unknown map id: ${mapId}`);
 
-const placePath = path.join(process.cwd(), target.file);
-const readLua = (file) => fs.readFileSync(path.join(process.cwd(), file), 'utf8').replaceAll(']]>', ']]]]><![CDATA[>');
+const placePath = path.join(root, target.file);
+const readLua = (file) => fs.readFileSync(path.join(root, file), 'utf8').replaceAll(']]>', ']]]]><![CDATA[>');
 
-const coreLua = readLua('maps/a-club/bbya.core.server.lua');
-const rebuildLua = readLua('maps/a-club/bbya.visual-rebuild-v4.server.lua');
-const polishLua = readLua('maps/a-club/bbya.visual-polish-v4.server.lua');
-const phase3Lua = readLua('maps/a-club/bbya.phase3-premium.server.lua');
-const phase4Lua = readLua('maps/a-club/bbya.phase4-experience.server.lua');
-const phase5Lua = readLua('maps/a-club/bbya.phase5-finish.server.lua');
-const phase6Lua = readLua('maps/a-club/bbya.phase6-wayfinding.server.lua');
-const productionQCLua = readLua('maps/a-club/bbya.production-qc-v4.server.lua');
-const liveFixLua = readLua('maps/a-club/bbya.livefix-4.7.server.lua');
-const lobbyLua = readLua('maps/a-club/bbya.lobby-reference-v4.8.server.lua');
-const buildValidationLua = readLua('maps/a-club/bbya.build-validation.server.lua');
-const playtestLua = readLua('maps/a-club/bbya.playtest.server.lua');
-const systemsLua = readLua('maps/a-club/bbya.systems.server.lua');
-const monetizationLua = readLua('maps/a-club/bbya.monetization.server.lua');
-const musicLua = readLua('maps/a-club/bbya.music.server.lua');
-const supportPanelLua = readLua('maps/a-club/bbya.support-panel.server.lua');
-const djLua = readLua('maps/a-club/bbya.dj.server.lua');
-const titleSizeLua = readLua('maps/a-club/bbya.title-size.server.lua');
-const rankSystemLua = readLua('maps/a-club/bbya.rank-system.server.lua');
-const queenAccessLua = readLua('maps/a-club/bbya.queen-access-hotfix.server.lua');
-const spawnFinalLua = readLua('maps/a-club/bbya.spawn-final.server.lua');
+const serverScripts = [
+  ['BBYA_Clean_Functional_Core_v3', 'maps/a-club/bbya.core.server.lua'],
+  ['BBYA_Premium_Visual_Rebuild_v4', 'maps/a-club/bbya.visual-rebuild-v4.server.lua'],
+  ['BBYA_Premium_Venue_Polish_v4_1', 'maps/a-club/bbya.visual-polish-v4.server.lua'],
+  ['BBYA_Premium_Phase_3_v4_3', 'maps/a-club/bbya.phase3-premium.server.lua'],
+  ['BBYA_Premium_Phase_4_v4_4_1', 'maps/a-club/bbya.phase4-experience.server.lua'],
+  ['BBYA_Premium_Phase_5_v4_5_1', 'maps/a-club/bbya.phase5-finish.server.lua'],
+  ['BBYA_Premium_Phase_6_v4_6', 'maps/a-club/bbya.phase6-wayfinding.server.lua'],
+  ['BBYA_Production_QC_v4_3', 'maps/a-club/bbya.production-qc-v4.server.lua'],
+  ['BBYA_Live_Playtest_Fix_v4_7', 'maps/a-club/bbya.livefix-4.7.server.lua'],
+  // v4.9 deliberately waits for builders above, then removes their old lobby geometry.
+  ['BBYA_Front_Lobby_Final_v4_9', 'maps/a-club/bbya.front-lobby-v4.9.server.lua'],
+  ['BBYA_Build_Validation', 'maps/a-club/bbya.build-validation.server.lua'],
+  ['BBYA_Queen_Playtest_System_Test_v1', 'maps/a-club/bbya.playtest.server.lua'],
+  ['BBYA_Functional_Systems_v2', 'maps/a-club/bbya.systems.server.lua'],
+  ['BBYA_Monetization_Backend', 'maps/a-club/bbya.monetization.server.lua'],
+  ['BBYA_Master_Music_Vault', 'maps/a-club/bbya.music.server.lua'],
+  ['BBYA_Top_Supporter_Data', 'maps/a-club/bbya.support-panel.server.lua'],
+  ['BBYA_Resident_DJ', 'maps/a-club/bbya.dj.server.lua'],
+  ['BBYA_Title_Size', 'maps/a-club/bbya.title-size.server.lua'],
+  ['BBYA_Social_Rank_System', 'maps/a-club/bbya.rank-system.server.lua'],
+  ['BBYA_Queen_Access', 'maps/a-club/bbya.queen-access-hotfix.server.lua'],
+  ['BBYA_Final_Arrival_Spawn', 'maps/a-club/bbya.spawn-final.server.lua'],
+];
 
-const clientLua = readLua('maps/a-club/bbya.client.lua');
-const musicClientLua = readLua('maps/a-club/bbya.music.client.lua');
-const supportPanelClientLua = readLua('maps/a-club/bbya.support-panel.client.lua');
-const monetizationClientLua = readLua('maps/a-club/bbya.monetization.client.lua');
-const supportCelebrationClientLua = readLua('maps/a-club/bbya.support-celebration.client.lua');
-const performanceClientLua = readLua('maps/a-club/bbya.performance.client.lua');
-const uiCoordinatorClientLua = readLua('maps/a-club/bbya.ui-coordinator.client.lua');
-const liveFixClientLua = readLua('maps/a-club/bbya.livefix-4.7.client.lua');
-const healthClientLua = readLua('maps/a-club/bbya.health.client.lua');
-const queenClientLua = readLua('maps/a-club/bbya.queen.client.lua');
+const clientScripts = [
+  ['BBYA_Dance_Studio_Client', 'maps/a-club/bbya.client.lua'],
+  ['BBYA_Music_Client', 'maps/a-club/bbya.music.client.lua'],
+  ['BBYA_Supporter_Client', 'maps/a-club/bbya.support-panel.client.lua'],
+  ['BBYA_Monetization_Client', 'maps/a-club/bbya.monetization.client.lua'],
+  ['BBYA_Support_Celebration_Client', 'maps/a-club/bbya.support-celebration.client.lua'],
+  ['BBYA_Adaptive_Performance_Client', 'maps/a-club/bbya.performance.client.lua'],
+  ['BBYA_UI_Coordinator_Client', 'maps/a-club/bbya.ui-coordinator.client.lua'],
+  ['BBYA_Live_Mobile_UI_Fix_v4_7', 'maps/a-club/bbya.livefix-4.7.client.lua'],
+  ['BBYA_Queen_Playtest_Health_HUD', 'maps/a-club/bbya.health.client.lua'],
+  ['BBYA_Queen_Client', 'maps/a-club/bbya.queen.client.lua'],
+];
+
+const scriptItem = (klass, name, file, ref) => `
+<Item class="${klass}" referent="${ref}">
+  <Properties>
+    <bool name="Disabled">false</bool>
+    <string name="Name">${name}</string>
+    <ProtectedString name="Source"><![CDATA[${readLua(file)}]]></ProtectedString>
+  </Properties>
+</Item>`;
+
+const serverItems = serverScripts.map((s, i) => scriptItem('Script', s[0], s[1], `RBXBBYASRV${String(i).padStart(4, '0')}`)).join('');
+const clientItems = clientScripts.map((s, i) => scriptItem('LocalScript', s[0], s[1], `RBXBBYACLI${String(i).padStart(4, '0')}`)).join('');
 
 let xml = fs.readFileSync(placePath, 'utf8');
 const begin = '<!-- BBYA_RUNTIME_BEGIN -->';
 const end = '<!-- BBYA_RUNTIME_END -->';
-const prior = new RegExp(`${begin}[\\s\\S]*?${end}`, 'g');
-xml = xml.replace(prior, '');
+xml = xml.replace(new RegExp(`${begin}[\\s\\S]*?${end}`, 'g'), '');
 
 const runtime = `${begin}
-<Item class="ServerScriptService" referent="RBXBBYASERVERSCRIPTSERVICE00000001">
-  <Properties><string name="Name">ServerScriptService</string></Properties>
-  <Item class="Script" referent="RBXBBYACLEANCOREV30000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Clean_Functional_Core_v3</string><ProtectedString name="Source"><![CDATA[${coreLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAVISUALREBUILDV4000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Premium_Visual_Rebuild_v4</string><ProtectedString name="Source"><![CDATA[${rebuildLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAVISUALPOLISHV4100000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Premium_Venue_Polish_v4_1</string><ProtectedString name="Source"><![CDATA[${polishLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAPHASE3PREMIUMV430000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Premium_Phase_3_v4_3</string><ProtectedString name="Source"><![CDATA[${phase3Lua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAPHASE4PREMIUMV440000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Premium_Phase_4_v4_4_1</string><ProtectedString name="Source"><![CDATA[${phase4Lua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAPHASE5PREMIUMV450000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Premium_Phase_5_v4_5_1</string><ProtectedString name="Source"><![CDATA[${phase5Lua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAPHASE6PREMIUMV460000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Premium_Phase_6_v4_6</string><ProtectedString name="Source"><![CDATA[${phase6Lua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAPRODUCTIONQCV4300000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Production_QC_v4_3</string><ProtectedString name="Source"><![CDATA[${productionQCLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYALIVEFIXV470000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Live_Playtest_Fix_v4_7</string><ProtectedString name="Source"><![CDATA[${liveFixLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYALOBBYV48000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Front_Lobby_Reference_v4_8</string><ProtectedString name="Source"><![CDATA[${lobbyLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYABUILDVALIDATION00000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Build_Validation_v1_3</string><ProtectedString name="Source"><![CDATA[${buildValidationLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAPLAYTESTSERVICE00000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Queen_Playtest_System_Test_v1</string><ProtectedString name="Source"><![CDATA[${playtestLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYASYSTEMS00000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Functional_Systems_v2</string><ProtectedString name="Source"><![CDATA[${systemsLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAMONETIZATION000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Monetization_Backend</string><ProtectedString name="Source"><![CDATA[${monetizationLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAMUSICSERVER0000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Master_Music_Vault</string><ProtectedString name="Source"><![CDATA[${musicLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYASUPPORTDATA0000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Top_Supporter_Data</string><ProtectedString name="Source"><![CDATA[${supportPanelLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYADJ00000000000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Resident_DJ</string><ProtectedString name="Source"><![CDATA[${djLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYATITLESIZE000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Title_Size</string><ProtectedString name="Source"><![CDATA[${titleSizeLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYARANKSYSTEM00000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Social_Rank_System</string><ProtectedString name="Source"><![CDATA[${rankSystemLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAQUEENACCESS000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Queen_Access</string><ProtectedString name="Source"><![CDATA[${queenAccessLua}]]></ProtectedString></Properties></Item>
-  <Item class="Script" referent="RBXBBYAFINALSPAWN0000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Final_Arrival_Spawn</string><ProtectedString name="Source"><![CDATA[${spawnFinalLua}]]></ProtectedString></Properties></Item>
+<Item class="ServerScriptService" referent="RBXBBYASSSFINAL">
+  <Properties><string name="Name">ServerScriptService</string></Properties>${serverItems}
 </Item>
-<Item class="StarterPlayer" referent="RBXBBYASTARTERPLAYER00000000000001">
+<Item class="StarterPlayer" referent="RBXBBYASPFINAL">
   <Properties><string name="Name">StarterPlayer</string></Properties>
-  <Item class="StarterPlayerScripts" referent="RBXBBYASTARTERPLAYERSCRIPTS00000001">
-    <Properties><string name="Name">StarterPlayerScripts</string></Properties>
-    <Item class="LocalScript" referent="RBXBBYACLIENT000000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Dance_Studio_Client</string><ProtectedString name="Source"><![CDATA[${clientLua}]]></ProtectedString></Properties></Item>
-    <Item class="LocalScript" referent="RBXBBYAMUSICCLIENT000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Music_Client</string><ProtectedString name="Source"><![CDATA[${musicClientLua}]]></ProtectedString></Properties></Item>
-    <Item class="LocalScript" referent="RBXBBYASUPPORTCLIENT0000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Supporter_Client</string><ProtectedString name="Source"><![CDATA[${supportPanelClientLua}]]></ProtectedString></Properties></Item>
-    <Item class="LocalScript" referent="RBXBBYAMONETIZATIONCLIENT00000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Monetization_Client</string><ProtectedString name="Source"><![CDATA[${monetizationClientLua}]]></ProtectedString></Properties></Item>
-    <Item class="LocalScript" referent="RBXBBYASUPPORTCELEBRATION00000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Support_Celebration_Client</string><ProtectedString name="Source"><![CDATA[${supportCelebrationClientLua}]]></ProtectedString></Properties></Item>
-    <Item class="LocalScript" referent="RBXBBYAPERFORMANCECLIENT0000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Adaptive_Performance_Client</string><ProtectedString name="Source"><![CDATA[${performanceClientLua}]]></ProtectedString></Properties></Item>
-    <Item class="LocalScript" referent="RBXBBYAUICOORDINATOR0000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_UI_Coordinator_Client</string><ProtectedString name="Source"><![CDATA[${uiCoordinatorClientLua}]]></ProtectedString></Properties></Item>
-    <Item class="LocalScript" referent="RBXBBYALIVEFIXCLIENT000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Live_Mobile_UI_Fix_v4_7</string><ProtectedString name="Source"><![CDATA[${liveFixClientLua}]]></ProtectedString></Properties></Item>
-    <Item class="LocalScript" referent="RBXBBYAHEALTHCLIENT000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Queen_Playtest_Health_HUD_v1_1</string><ProtectedString name="Source"><![CDATA[${healthClientLua}]]></ProtectedString></Properties></Item>
-    <Item class="LocalScript" referent="RBXBBYAQUEENCLIENT0000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Queen_Client</string><ProtectedString name="Source"><![CDATA[${queenClientLua}]]></ProtectedString></Properties></Item>
+  <Item class="StarterPlayerScripts" referent="RBXBBYASPSFINAL">
+    <Properties><string name="Name">StarterPlayerScripts</string></Properties>${clientItems}
   </Item>
 </Item>
 ${end}`;
@@ -96,4 +82,4 @@ ${end}`;
 if (!xml.includes('</roblox>')) throw new Error('Invalid RBXLX: missing </roblox>');
 xml = xml.replace('</roblox>', `${runtime}</roblox>`);
 fs.writeFileSync(placePath, xml);
-console.log('[BBYA] Active build injected: Premium 4.8 front-lobby build + crown neon identity + warm social lobby + livefix 4.7 + validation into', target.file);
+console.log('[BBYA] Active build injected: v4.9 clean front lobby AFTER legacy builders; v4.8 retired.');
