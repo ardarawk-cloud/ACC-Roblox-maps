ae4ba7645bcbbd69dc7a197534db600b6732d77c
ardarawk-cloud <ardarawk@gmail.com>
2026-08-19 01:22:58 +0800

    Add BBYA V5 zone D3 sky bar

---
+++ maps/a-club/v5/D3-skybar.lua
@@ -0,0 +1,4 @@
+-- [D3] SKY BAR PROGRAM
+local D3=registerZone("D3","SKY BAR PROGRAM","ROOF",Vector3.new(-58,36.72,43),Vector3.new(40,.14,34))
+program(D3,"SKY BAR PROGRAM ZONE",Vector2.new(40,34),Vector3.new(-58,36.72,43),C.gold)
+zoneStamp(D3,CFrame.new(-58,42,60),Vector3.new(22,3,.25),C.gold,Enum.NormalId.Front)
