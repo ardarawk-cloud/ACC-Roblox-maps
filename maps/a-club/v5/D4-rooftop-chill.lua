310178ff86ddb4bbe2c18b6734504dac50e320a6
ardarawk-cloud <ardarawk@gmail.com>
2026-08-19 01:23:07 +0800

    Add BBYA V5 zone D4 rooftop chill

---
+++ maps/a-club/v5/D4-rooftop-chill.lua
@@ -0,0 +1,4 @@
+-- [D4] ROOFTOP CHILL PROGRAM
+local D4=registerZone("D4","ROOFTOP CHILL PROGRAM","ROOF",Vector3.new(58,36.72,43),Vector3.new(40,.14,34))
+program(D4,"ROOFTOP CHILL PROGRAM ZONE",Vector2.new(40,34),Vector3.new(58,36.72,43),C.cyan)
+zoneStamp(D4,CFrame.new(58,42,60),Vector3.new(26,3,.25),C.cyan,Enum.NormalId.Front)
