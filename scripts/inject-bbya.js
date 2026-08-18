8cda41f02032af0e92917e8b3d66d358c556b135
ardarawk-cloud <ardarawk@gmail.com>
2026-08-18 18:52:05 +0800

    BBYA: inject supporter data and avatar panel

--- scripts/inject-bbya.js
+++ scripts/inject-bbya.js
@@ -14,6 +14,7 @@ const mainLua = readLua('maps/a-club/bbya.server.lua');
 const architectureLua = readLua('maps/a-club/bbya.architecture.server.lua');
 const systemsLua = readLua('maps/a-club/bbya.systems.server.lua');
 const musicLua = readLua('maps/a-club/bbya.music.server.lua');
+const supportPanelLua = readLua('maps/a-club/bbya.support-panel.server.lua');
 const djLua = readLua('maps/a-club/bbya.dj.server.lua');
 const featuresLua = readLua('maps/a-club/bbya.features.server.lua');
 const qcLua = readLua('maps/a-club/bbya.qc.server.lua');
@@ -26,6 +27,7 @@ const spatialCleanupLua = readLua('maps/a-club/bbya.spatial-cleanup.server.lua')
 const queenAccessLua = readLua('maps/a-club/bbya.queen-access-hotfix.server.lua');
 const clientLua = readLua('maps/a-club/bbya.client.lua');
 const musicClientLua = readLua('maps/a-club/bbya.music.client.lua');
+const supportPanelClientLua = readLua('maps/a-club/bbya.support-panel.client.lua');
 const queenClientLua = readLua('maps/a-club/bbya.queen.client.lua');
 
 let xml = fs.readFileSync(placePath, 'utf8');
@@ -41,6 +43,7 @@ const runtime = `${begin}
   <Item class="Script" referent="RBXBBYAARCHITECTURE000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Mega_Architecture_v2</string><ProtectedString name="Source"><![CDATA[${architectureLua}]]></ProtectedString></Properties></Item>
   <Item class="Script" referent="RBXBBYASYSTEMS00000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Functional_Systems</string><ProtectedString name="Source"><![CDATA[${systemsLua}]]></ProtectedString></Properties></Item>
   <Item class="Script" referent="RBXBBYAMUSICSERVER0000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Master_Music_Vault</string><ProtectedString name="Source"><![CDATA[${musicLua}]]></ProtectedString></Properties></Item>
+  <Item class="Script" referent="RBXBBYASUPPORTDATA0000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Top_Supporter_Data</string><ProtectedString name="Source"><![CDATA[${supportPanelLua}]]></ProtectedString></Properties></Item>
   <Item class="Script" referent="RBXBBYADJ00000000000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Resident_DJ</string><ProtectedString name="Source"><![CDATA[${djLua}]]></ProtectedString></Properties></Item>
   <Item class="Script" referent="RBXBBYAFEATURES0000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Feature_Stations</string><ProtectedString name="Source"><![CDATA[${featuresLua}]]></ProtectedString></Properties></Item>
   <Item class="Script" referent="RBXBBYAQC000000000000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_QC_Hotfix</string><ProtectedString name="Source"><![CDATA[${qcLua}]]></ProtectedString></Properties></Item>
@@ -58,6 +61,7 @@ const runtime = `${begin}
     <Properties><string name="Name">StarterPlayerScripts</string></Properties>
     <Item class="LocalScript" referent="RBXBBYACLIENT000000000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Client</string><ProtectedString name="Source"><![CDATA[${clientLua}]]></ProtectedString></Properties></Item>
     <Item class="LocalScript" referent="RBXBBYAMUSICCLIENT000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Music_Client</string><ProtectedString name="Source"><![CDATA[${musicClientLua}]]></ProtectedString></Properties></Item>
+    <Item class="LocalScript" referent="RBXBBYASUPPORTCLIENT0000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Supporter_Client</string><ProtectedString name="Source"><![CDATA[${supportPanelClientLua}]]></ProtectedString></Properties></Item>
     <Item class="LocalScript" referent="RBXBBYAQUEENCLIENT0000000000000001"><Properties><bool name="Disabled">false</bool><string name="Name">BBYA_Queen_Client</string><ProtectedString name="Source"><![CDATA[${queenClientLua}]]></ProtectedString></Properties></Item>
   </Item>
 </Item>
@@ -66,4 +70,4 @@ ${end}`;
 if (!xml.includes('</roblox>')) throw new Error('Invalid RBXLX: missing </roblox>');
 xml = xml.replace('</roblox>', `${runtime}</roblox>`);
 fs.writeFileSync(placePath, xml);
-console.log('[BBYA] Active build injected: architecture + systems + Music Vault/Auto-DJ + DJ + features + QC + social ranks + visual/layout/Queen hotfixes + mobile panels into', target.file);
+console.log('[BBYA] Active build injected: architecture + systems + Music Vault/Auto-DJ + Top Supporter avatar panel + DJ + features + QC + social ranks + visual/layout/Queen hotfixes + mobile panels into', target.file);
