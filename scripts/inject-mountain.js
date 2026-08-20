81076530e32cb45fa88695d60bcce37aeb255720
ardarawk-cloud <ardarawk@gmail.com>
2026-08-20 17:22:10 +0800

    simplify Mountain injector for Roblox content compatibility

--- scripts/inject-mountain.js
+++ scripts/inject-mountain.js
@@ -14,48 +14,17 @@ if (String(target.universeId) !== '10744139279' || String(target.placeId) !== '8
 
 const placePath = path.join(root, target.file);
 const readLua = (file) => fs.readFileSync(path.join(root, file), 'utf8').replaceAll(']]>', ']]]]><![CDATA[>');
+const worldLua = readLua('maps/mountain-social/mountain.world.server.lua');
 
-const serverFiles = [
-  ['ACC_Mountain_World', 'maps/mountain-social/mountain.world.server.lua'],
-  ['ACC_Mountain_Checkpoints', 'maps/mountain-social/systems/checkpoint.server.lua'],
-  ['ACC_Mountain_Ambience', 'maps/mountain-social/systems/ambience.server.lua'],
-  ['ACC_Mountain_Carry', 'maps/mountain-social/systems/carry.server.lua'],
-  ['ACC_Mountain_Summit', 'maps/mountain-social/systems/summit.server.lua'],
-  ['ACC_Mountain_Interactions', 'maps/mountain-social/mountain.interactions.server.lua'],
-  ['ACC_Mountain_QC', 'maps/mountain-social/mountain.qc.server.lua'],
-];
-const clientFiles = [
-  ['ACC_Mountain_Client', 'maps/mountain-social/mountain.client.lua'],
-  ['ACC_Mountain_Performance', 'maps/mountain-social/mountain.performance.client.lua'],
-];
-
-let xml = fs.readFileSync(placePath, 'utf8');
-const begin = '<!-- ACC_MOUNTAIN_RUNTIME_BEGIN -->';
-const end = '<!-- ACC_MOUNTAIN_RUNTIME_END -->';
-const prior = new RegExp(`${begin}[\\s\\S]*?${end}`, 'g');
-xml = xml.replace(prior, `${begin}\n${end}`);
-
-const scriptItem = (name, source, i) => `  <Item class="Script" referent="RBXMOUNTAINSERVER${String(i).padStart(4,'0')}"><Properties><bool name="Disabled">false</bool><string name="Name">${name}</string><ProtectedString name="Source"><![CDATA[${source}]]></ProtectedString></Properties></Item>`;
-const localItem = (name, source, i) => `    <Item class="LocalScript" referent="RBXMOUNTAINCLIENT${String(i).padStart(4,'0')}"><Properties><bool name="Disabled">false</bool><string name="Name">${name}</string><ProtectedString name="Source"><![CDATA[${source}]]></ProtectedString></Properties></Item>`;
-
-const servers = serverFiles.map(([name,file],i)=>scriptItem(name,readLua(file),i+1)).join('\n');
-const clients = clientFiles.map(([name,file],i)=>localItem(name,readLua(file),i+1)).join('\n');
-
-const runtime = `${begin}
-<Item class="ServerScriptService" referent="RBXMOUNTAINSERVERSERVICE000000001">
-  <Properties><string name="Name">ServerScriptService</string></Properties>
-${servers}
+const xml = `<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.roblox.com/roblox.xsd" version="4">
+<External>null</External><External>nil</External>
+<Item class="Workspace" referent="RBXMOUNTAINWORKSPACE"><Properties><string name="Name">Workspace</string></Properties></Item>
+<Item class="Lighting" referent="RBXMOUNTAINLIGHTING"><Properties><float name="Brightness">2</float><double name="ClockTime">6.2</double><string name="Name">Lighting</string></Properties></Item>
+<Item class="ServerScriptService" referent="RBXMOUNTAINSSS"><Properties><string name="Name">ServerScriptService</string></Properties>
+<Item class="Script" referent="RBXMOUNTAINBUILD"><Properties><string name="Name">ACC_Mountain_World</string><bool name="Disabled">false</bool><ProtectedString name="Source"><![CDATA[${worldLua}]]></ProtectedString></Properties></Item>
+<Item class="Script" referent="RBXMOUNTAINQC"><Properties><string name="Name">ACC_Mountain_QC</string><bool name="Disabled">false</bool><ProtectedString name="Source"><![CDATA[task.delay(8,function() workspace:SetAttribute('ACC_MountainReady', workspace:FindFirstChild('ACC_MountainSocial') ~= nil) end)]]></ProtectedString></Properties></Item>
 </Item>
-<Item class="StarterPlayer" referent="RBXMOUNTAINSTARTERPLAYER000000001">
-  <Properties><string name="Name">StarterPlayer</string></Properties>
-  <Item class="StarterPlayerScripts" referent="RBXMOUNTAINSTARTERSCRIPTS00000001">
-    <Properties><string name="Name">StarterPlayerScripts</string></Properties>
-${clients}
-  </Item>
-</Item>
-${end}`;
+</roblox>`;
 
-if (!xml.includes('</roblox>')) throw new Error('Invalid Mountain RBXLX: missing </roblox>');
-xml = xml.replace(`${begin}\n${end}`, runtime);
 fs.writeFileSync(placePath, xml);
-console.log('[Mountain] Injected isolated runtime into', target.file);
+console.log('[Mountain] Injected minimal BBYA-compatible Mountain runtime into', target.file);
