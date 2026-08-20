07227c9239499febad79df7504c5bf0b0dcc83b9
ardarawk-cloud <ardarawk@gmail.com>
2026-08-20 18:30:45 +0800

    add Mountain visual polish module

---
+++ maps/mountain-social/mountain.visual.server.lua
@@ -0,0 +1,5 @@
+-- Mountain Social Adventure visual polish module v1.4
+local Workspace=game:GetService("Workspace")
+local Terrain=Workspace.Terrain
+local root=Workspace:WaitForChild("ACC_MountainSocial")
+root:SetAttribute("VisualPolish","1.4")
