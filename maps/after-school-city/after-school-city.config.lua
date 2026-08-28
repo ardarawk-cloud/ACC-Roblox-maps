fe02194f877c5186dd3d60e105c61ea886384397
ardarawk-cloud <ardarawk@gmail.com>
2026-08-29 00:03:28 +0800

    Wire After School City V0.7.1 polish config

--- maps/after-school-city/after-school-city.config.lua
+++ maps/after-school-city/after-school-city.config.lua
@@ -1,7 +1,7 @@
 local Config = {
     ProjectId = "after-school-city",
     DisplayName = "AFTER SCHOOL CITY",
-    Version = "0.7.0-school-interior-1",
+    Version = "0.7.1-school-interior-polish-1",
     OrientationBaseVersion = "0.4.7-orientation-correction-1",
     VisualFidelityVersion = "0.5.0-visual-fidelity-1",
     RuntimeReconcileVersion = "0.5.1-runtime-reconcile-1",
@@ -9,6 +9,7 @@ local Config = {
     RoadCenterlineClearanceVersion = "0.5.3-road-centerline-clearance-1",
     PremiumExteriorVersion = "0.6.0-premium-exterior-1",
     SchoolInteriorVersion = "0.7.0-school-interior-1",
+    SchoolInteriorPolishVersion = "0.7.1-school-interior-polish-1",
 
     Roblox = {
         UniverseId = "10745359869",
@@ -55,6 +56,7 @@ local Config = {
         EnableRoadCenterlineClearancePass = true,
         EnablePremiumExteriorPass = true,
         EnableSchoolInteriorPass = true,
+        EnableSchoolInteriorPolishPass = true,
         EnableDebugBillboards = false,
         EnableActivities = false,
         EnableEconomy = false,
